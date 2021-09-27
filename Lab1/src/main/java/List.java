d6bb6188e53f0ddc7db91f7c875b6e9d0701c363
HackSider27 <a.Danila2001@ya.ru>
2021-09-27 15:59:23 +0300

    fb

---
+++ Lab1/src/main/java/List.java
@@ -0,0 +1,110 @@
+import java.security.cert.TrustAnchor;
+import java.util.Scanner;
+
+public class List{
+    private Object[] list;
+    private int size;
+
+    List(){
+        size=0;
+    }
+
+    List(Object a) {
+        size=1;
+        list = new Object[size];
+        list[0] = a;
+    }
+
+    List(Object[] a) {
+        size=a.length;
+        list = new Object[a.length];
+        list = a;
+    }
+
+    void add(Object val) {
+        size++;
+        Object[] new_list = new Object[size];
+        for (int i = 0; i < size-1; i++)
+            new_list[i] = list[i];
+        new_list[size-1] = val;
+        list = new_list;
+    }
+
+    void add(Object val, int ind){
+        if(ind >= size) {
+            ind = size;
+            add(val);
+            return;
+        }
+
+        size++;
+        Object[] help_list = new Object[size];
+        for (int i = 0, j=0; i < size; i++, j++) {
+            if(i != ind)
+                help_list[i] = list[j];
+            else{
+                help_list[i]=val;
+                i++;
+            }
+        }
+        help_list[size-1] = val;
+        list = help_list;
+    }
+
+    Object remove(int index){
+
+        if(index > size)
+            return null;
+
+        Object[] upd_list = new Object[size-1];
+        int j = 0;
+        Object res = list[index];
+        for (int i = 0; i < size; i++){
+            if(i == index)
+                continue;
+            upd_list[j] = list[i];
+            j++;
+        }
+        list = new Object[size--];
+        list = upd_list;
+        return res;
+    }
+
+    Object get(int index){
+        if (index > size)
+            return null;
+        else return list[index];
+    }
+
+    boolean contains(Object value){
+        for(int i = 0; i<size; i++){
+            if (list[i] == value)
+                return true;
+        }
+        return false;
+    }
+
+    boolean isEmpty(){
+        if(size < 1)
+            return false;
+        return true;
+    }
+
+    int Size(){
+        return size;
+    }
+
+    int indexOf(Object value){
+        int i = 0;
+        while(list[i] != value)
+            i++;
+        return i;
+    }
+
+    void set(Object value, int index){
+        list[index]=value;
+    }
+}
+
+
+
---
+++ Lab1/src/main/java/Map.java
@@ -0,0 +1,140 @@
+public class Map{
+    public static class Node{
+        public Object key;
+        public Object value;
+
+        Node(){
+            key = null;
+            value = null;
+        }
+
+        Node(Object k, Object val){
+            value = val;
+            key = k;
+        }
+    }
+
+    private int size;
+    private Node[] list;
+
+    Map(){
+        size = 0;
+    }
+
+    Map(Node a){
+        size = 1;
+        list = new Node[1];
+        list[0] = a;
+    }
+
+    Map(int s, Node... nodes){
+        size = s;
+        list = nodes;
+    }
+
+    public void put(Object k, Object val){
+        for(int i = 0; i < size; i++){
+            if(list[i].key == k) {
+                list[i].value = val;
+                return;
+            }
+        }
+        Node[] new_list = new Node[++size];
+
+        for(int i = 0; i < size-1; i++)
+            new_list[i] = list[i];
+
+        Node new_node = new Node(k, val);
+        new_list[size-1] = new_node;
+        list = new Node[size];
+        list = new_list;
+    }
+
+    public Object get(Object k){
+        int i = 0;
+        while(i < size){
+            if (list[i].key == k)
+                return list[i].value;
+            i++;
+        }
+        return null;
+    }
+
+    public Object get(Object k, Object bydefault){
+        for(int i = 0; i < size; i++){
+            if(list[i].key == k) {
+                if (list[i].value != null)
+                    return list[i].value;
+                else return bydefault;
+            }
+        }
+        return null;
+    }
+
+    public Object remove(Object k){
+        Object val = null;
+        for(int i = 0; i < size; i++){
+            if(list[i].key == k) {
+                val = list[i].value;
+                break;
+            }
+        }
+        if(val != null){
+            Node[] upd_array = new Node[size-1];
+            int j = 0;
+            for (int i = 0; i < size; i++){
+                if(list[i].key == k)
+                    continue;
+                upd_array[j] = list[i];
+                j++;
+            }
+            list = new Node[size--];
+            list = upd_array;
+            return val;
+        }
+        return val;
+    }
+
+    public boolean keyContains(Object k){
+        for(int i = 0; i < size; i++){
+            if(list[i].key == k)
+                return true;
+        }
+        return false;
+    }
+
+    public List getKeys(){
+        List l = new List();
+        for(int i = 0; i < size; i++){
+            l.add(list[i].key);
+        }
+        return l;
+    }
+
+    public List getValues(){
+        List l = new List();
+        for(int i = 0; i < size; i++){
+            l.add(list[i].value);
+        }
+        return l;
+    }
+
+    public List getEntries(){
+        List l = new List();
+        for(int i = 0; i < size; i++){
+            l.add(list[i]);
+        }
+        return l;
+    }
+
+    public int getSize(){
+        return size;
+    }
+
+    public boolean isEmpty(){
+        if(size > 0)
+            return false;
+        else return true;
+    }
+
+}
